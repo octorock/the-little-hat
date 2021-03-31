e22de7b7965d590a2a98ce314eb5f59f466466a3
octorock <79596758+octorock@users.noreply.github.com>
2021-03-31 18:29:04 +0200

    Add first few constraint tests

--- tests/test_constaints.py
+++ tests/test_constaints.py
@@ -4,32 +4,41 @@ from tlh.const import RomVariant
 from tlh.data.constraints import Constraint, ConstraintManager, RomVariantNotAddedError
 import pytest
 
-def assert_same_offset(manager: ConstraintManager, rom: RomVariant, offset: int) -> None:
-    assert offset == manager.to_virtual(rom, offset)
-    assert offset == manager.to_local(rom, offset)
+# The tables in this file are showing the configuration of the constraints
+# The headers have the following meaning:
+# v: virtual address
+# U: RomVariation.USA
+# D: RomVariation.DEMO
+# E: RomVariation.EU
+# J: RomVariation.JP
+# - between numbers denotes that these two are connected with a constraint
 
-def assert_differing_offset(manager: ConstraintManager, rom: RomVariant, local_offset: int, virtual_offset: int) -> None:
-    assert virtual_offset == manager.to_virtual(rom, local_offset)
-    assert local_offset == manager.to_local(rom, virtual_offset)
+def assert_same_address(manager: ConstraintManager, rom: RomVariant, address: int) -> None:
+    assert address == manager.to_virtual(rom, address)
+    assert address == manager.to_local(rom, address)
+
+def assert_differing_address(manager: ConstraintManager, rom: RomVariant, local_address: int, virtual_address: int) -> None:
+    assert virtual_address == manager.to_virtual(rom, local_address)
+    assert local_address == manager.to_local(rom, virtual_address)
 
 def test_no_constaints():
     manager = ConstraintManager({RomVariant.USA, RomVariant.EU})
-    assert_same_offset(manager, RomVariant.USA, 0)
-    assert_same_offset(manager, RomVariant.EU, 0)
-    assert_same_offset(manager, RomVariant.USA, 1)
-    assert_same_offset(manager, RomVariant.EU, 1)
-    assert_same_offset(manager, RomVariant.USA, 100)
-    assert_same_offset(manager, RomVariant.EU, 100)
-    assert_same_offset(manager, RomVariant.USA, 0xffffff)
-    assert_same_offset(manager, RomVariant.EU, 0xffffff)
+    assert_same_address(manager, RomVariant.USA, 0)
+    assert_same_address(manager, RomVariant.EU, 0)
+    assert_same_address(manager, RomVariant.USA, 1)
+    assert_same_address(manager, RomVariant.EU, 1)
+    assert_same_address(manager, RomVariant.USA, 100)
+    assert_same_address(manager, RomVariant.EU, 100)
+    assert_same_address(manager, RomVariant.USA, 0xffffff)
+    assert_same_address(manager, RomVariant.EU, 0xffffff)
 
     with pytest.raises(RomVariantNotAddedError):
-        assert_same_offset(manager, RomVariant.JP, 0)
+        assert_same_address(manager, RomVariant.JP, 0)
     with pytest.raises(RomVariantNotAddedError):
-        assert_same_offset(manager, RomVariant.DEMO, 0)
+        assert_same_address(manager, RomVariant.DEMO, 0)
 
 def test_first_constraint():
-    # v  A B
+    # v  U D
     # 0  0 0
     # 1  x 1
     # 2  1-2 
@@ -38,16 +47,79 @@ def test_first_constraint():
     manager = ConstraintManager({RomVariant.USA, RomVariant.DEMO})
     constraint = Constraint()
     constraint.romA = RomVariant.USA
-    constraint.offsetA = 1
+    constraint.addressA = 1
     constraint.romB = RomVariant.DEMO
-    constraint.offsetB = 2
+    constraint.addressB = 2
     manager.add_constraint(constraint)
-    assert_same_offset(manager, RomVariant.USA, 0)
-    assert_same_offset(manager, RomVariant.DEMO, 0)
-    assert_same_offset(manager, RomVariant.DEMO, 1)
-    assert_same_offset(manager, RomVariant.DEMO, 2)
-    assert_same_offset(manager, RomVariant.DEMO, 3)
-    assert_same_offset(manager, RomVariant.DEMO, 0xffffff)
-    assert_differing_offset(manager, RomVariant.USA, 1, 2)
-    assert_differing_offset(manager, RomVariant.USA, 2, 3)
-    assert_differing_offset(manager, RomVariant.USA, 0xffffff, 0xffffff+1)
+    assert_same_address(manager, RomVariant.USA, 0)
+    assert_same_address(manager, RomVariant.DEMO, 0)
+    assert_same_address(manager, RomVariant.DEMO, 1)
+    assert_same_address(manager, RomVariant.DEMO, 2)
+    assert_same_address(manager, RomVariant.DEMO, 3)
+    assert_same_address(manager, RomVariant.DEMO, 0xffffff)
+    assert_differing_address(manager, RomVariant.USA, 1, 2)
+    assert_differing_address(manager, RomVariant.USA, 2, 3)
+    assert_differing_address(manager, RomVariant.USA, 0xffffff, 0xffffff+1)
+    print('---')
+    assert -1 == manager.to_local(RomVariant.USA, 1)
+
+def test_longer_constraint():
+    # v   E   J
+    # 0   0   0
+    # 1   x   1
+    # ... ... ...
+    # 100 1   100
+    # 101 2   101
+    manager = ConstraintManager({RomVariant.EU, RomVariant.JP})
+    constraint = Constraint()
+    constraint.romA = RomVariant.JP
+    constraint.addressA = 100
+    constraint.romB = RomVariant.EU
+    constraint.addressB = 1
+    manager.add_constraint(constraint)
+    assert_same_address(manager, RomVariant.EU, 0)
+    assert_same_address(manager, RomVariant.JP, 0)
+    assert_same_address(manager, RomVariant.JP, 100)
+    assert_same_address(manager, RomVariant.JP, 5000)
+    assert -1 == manager.to_local(RomVariant.EU, 1)
+    assert_differing_address(manager, RomVariant.EU, 1, 100)
+    assert_differing_address(manager, RomVariant.EU, 100, 199)
+
+
+def add_j_e_constraint(manager: ConstraintManager, jp_address: int, eu_address: int):
+    constraint = Constraint()
+    constraint.romA = RomVariant.JP
+    constraint.addressA = jp_address
+    constraint.romB = RomVariant.EU
+    constraint.addressB = eu_address
+    manager.add_constraint(constraint)
+
+#def test_two_constraints():
+    # v J E
+    # 0 0 0
+    # 1 x 1
+    # 2 1-2
+    # 3 2 x
+    # 4 3-3
+    # 5 4 4
+#    manager = ConstraintManager({RomVariant.JP, RomVariant.EU})
+#    add_j_e_constraint(manager, 1, 2)
+#    add_j_e_constraint(manager, 3, 3)
+
+#    assert_same_address(manager, RomVariant.EU, 1)
+#    assert_same_address(manager, RomVariant.EU, 2)
+#    assert_differing_address(manager, RomVariant.JP, 1, 2)
+#    assert_differing_address(manager, RomVariant.JP, 2, 3)
+#    assert_differing_address(manager, RomVariant.JP, 3, 4)
+#    assert_differing_address(manager, RomVariant.JP, 4, 5)
+#    assert_differing_address(manager, RomVariant.EU, 3, 4)
+#    assert_differing_address(manager, RomVariant.EU, 4, 5)
+
+# TODO
+# add two constraints
+# add constraints in the wrong order
+# add situation were local_address == next.local_address can occur (if it exists)
+# add situation with invalid constraint
+# add constraints between three files
+# add constraints between four files
+# add cyclic constraints?
--- tlh/data/constraints.py
+++ tlh/data/constraints.py
@@ -1,13 +1,66 @@
 from tlh.const import RomVariant
-
+from dataclasses import dataclass
 
 class Constraint:
+    """
+    A constraint defines that two local addresses of different rom variants should be at the same virtual address
+    """
     romA: RomVariant = None
-    offsetA: int = 0
+    addressA: int = 0
     romB: RomVariant = None
-    offsetB: int = 0
+    addressB: int = 0
     certainty: int = 0
     note: str = None
+
+class RomRelation:
+    def __init__(self, local_address: int, virtual_address: int):
+        self.local_address = local_address
+        self.virtual_address = virtual_address
+
+class RomRelations:
+    """
+    A rom relation defines the relation between a local address for this rom variant and the corresponding virtual address
+    """
+    def __init__(self, romVariant: RomVariant) -> None:
+        self.romVariant = romVariant
+        self.relations: list[RomRelation] = []
+    
+    def add_relation(self, local_address: int, virtual_address: int):
+        # TODO keep relations list sorted
+        self.relations.append(RomRelation(local_address, virtual_address))
+
+    def clear(self):
+        self.relations.clear()
+
+    def get_previous_relation_for_local_address(self, local_address: int) -> RomRelation:
+        prev = None
+
+        # TODO use binary search
+        for relation in self.relations:
+            if relation.local_address > local_address:
+                return prev
+            prev = relation
+
+        print('not found', local_address, prev, self.romVariant)
+        return prev
+
+    def get_prev_and_next_relation_for_virtual_address(self, virtual_address: int) -> (RomRelation, RomRelation):
+        prev = None
+        
+        # TODO use binary search
+        for relation in self.relations:
+            if relation.virtual_address > virtual_address:
+                print('PREV')
+                return (prev, relation)
+            prev = relation
+        print('last', virtual_address)
+        return (prev, None)
+
+@dataclass
+class Hint:
+    local_address: int
+    rom_variant: RomVariant
+    rom_address: int
 
 class ConstraintManager:
     def __init__(self, variants: set[RomVariant]) -> None:
@@ -15,10 +68,100 @@ class ConstraintManager:
         Pass a set of all RomVariants that should be linked using this constraint manager
         """
         self.variants = variants
-        self.constraints = []
+        self.constraints: list[Constraint] = []
+        self.rom_relations: dict[str, RomRelations] = {}
+        for variant in variants:
+            self.rom_relations[variant] = RomRelations(variant)
+            
+
 
     def add_constraint(self, constraint: Constraint) -> None:
-        self.constraints.append(constraint)
+        self.constraints.append(constraint) # TODO add at the correct place
+        self.rebuild_relations()
+
+    def rebuild_relations(self) -> None:
+        """
+        Builds relations between local addresses for each variation and the virtual address based on the constraints
+        """
+        virtual_address = 0
+        local_addresses: dict[str, int] = {}
+        local_hints: dict[str, Hint] = {}
+        for variant in self.variants:
+            local_addresses[variant] = 0
+            local_hints[variant] = []
+            self.rom_relations[variant].clear()
+
+
+        constraints = self.constraints.copy()
+
+        while constraints: # not empty
+
+            # get constraint with the lowest virtual address
+            # TODO is there a way to keep the list sorted by these virtual addresses which are constantly changing with each new constraint?
+            selected_constraint = constraints[0]
+            selected_variant = constraints[0].romA
+            selected_local_address = constraints[0].addressA
+            selected_virtual_address = self.to_virtual(selected_variant, selected_local_address)
+
+            for constraint in constraints:
+                # handle A
+                variant_a = constraint.romA
+                local_address_a = constraint.addressA
+                virtual_address_a = self.to_virtual(variant_a, local_address_a)
+                if virtual_address_a < selected_virtual_address:
+                    selected_constraint = constraint
+                    selected_variant = variant_a
+                    selected_local_address = local_address_a
+                    selected_virtual_address = virtual_address_a
+                # handle B
+                variant_b = constraint.romB
+                local_address_b = constraint.addressB
+                virtual_address_b = self.to_virtual(variant_b, local_address_b)
+                if virtual_address_b < selected_virtual_address:
+                    selected_constraint = constraint
+                    selected_variant = variant_b
+                    selected_local_address = local_address_b
+                    selected_virtual_address = virtual_address_b
+
+            # Remove constraint
+            constraints.remove(selected_constraint)
+
+            virtual_address_offset = selected_virtual_address - virtual_address
+            # Advance all local addresses to this virtual address
+            for variant in self.variants:
+                local_address = local_addresses[variant] + virtual_address_offset
+                local_addresses[variant] = local_address
+                # TODO handle hints
+
+            # for the lower address of the constraint, add a relation to the current virtual address
+            virtual_address_a = self.to_virtual(selected_variant, selected_constraint.addressA)
+            virtual_address_b = self.to_virtual(selected_variant, selected_constraint.addressB)
+            if virtual_address_a < virtual_address_b:
+                self.rom_relations[selected_constraint.romA].add_relation(selected_constraint.addressA, virtual_address_b)
+            else:
+                self.rom_relations[selected_constraint.romB].add_relation(selected_constraint.addressB, virtual_address_a)
+            # TODO addresses are the same -> insert both constraints here
+
+        # while constraints exist
+            # get constraints that has the lowest virtual address
+            # advance all local addresses to this virtual address
+            # TODO if there are hints left until there, handle them
+            # for the lower address of the constraint, add a relation to the current virtual address
+            # add a hint for the higher address to be inserted later
+
+
+        # for constraint in self.constraints:
+        #     # Add relation to rom variant with the lower address
+        #     # TODO check the difference of the virtual constraints!
+        #     if constraint.addressA < constraint.addressB:
+        #         print('a')
+        #         virtual_address = constraint.addressB
+        #         self.rom_relations[constraint.romA].add_relation(constraint.addressA, virtual_address)
+        #     else:
+        #         print('b')
+        #         virtual_address = constraint.addressA
+        #         self.rom_relations[constraint.romB].add_relation(constraint.addressB, virtual_address)
+        #     # TODO continue until we are at the other constraint
 
     def to_local(self, rom: RomVariant, virtual_address: int) -> int:
         """
@@ -26,7 +169,28 @@ class ConstraintManager:
         """
         if not rom in self.variants:
             raise RomVariantNotAddedError()
-        return virtual_address
+
+        (prev, next) = self.rom_relations[rom].get_prev_and_next_relation_for_virtual_address(virtual_address)
+        if prev is None:
+            if next is not None:
+                if virtual_address >= next.local_address:
+                    # No local address at this virtual address
+                    return -1
+            return virtual_address
+        else:
+            # Calculate offset to virtual_address
+            offset = virtual_address - prev.virtual_address
+            local_address = prev.local_address + offset
+            print(prev.virtual_address)
+            if next is not None:
+                if local_address > next.local_address:
+                    # No local address at this virtual address
+                    return -1
+                elif local_address == next.local_address:
+                    # This should not happen, if it's equal, the relation should be the prev relation
+                    # TODO maybe this can happen, because only the virtual address needs to be prev?
+                    assert False
+            return local_address
 
     def to_virtual(self, rom: RomVariant, local_address: int) -> int:
         """
@@ -34,8 +198,21 @@ class ConstraintManager:
         """
         if not rom in self.variants:
             raise RomVariantNotAddedError()
-        return local_address
+
+        relation = self.rom_relations[rom].get_previous_relation_for_local_address(local_address)
+        if relation is None:
+            print('passing')
+            return local_address
+        else:
+            # Calculate offset to local_address
+            offset = local_address - relation.local_address
+            print('got offset', offset, relation.virtual_address)
+            return relation.virtual_address + offset
 
 
 class RomVariantNotAddedError(Exception):
+    pass
+
+# TODO pass the other constraint(s) it is invalid against?
+class InvalidConstraintError(Exception):
     pass
